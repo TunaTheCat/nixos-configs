1bc5f6f3920f001546a1e445d8dcb9e95f7ea63e
jb <jb@w.ch>
2026-05-07 16:36:18 +0200

    pimp audio service

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -3,7 +3,16 @@
   flake.modules.nixos.services = {
     services.tailscale.enable = true;
     services.openssh.enable = true;
-    services.pipewire.jack.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      wireplumber = {
+        enable = true;
+      };
+    };
 
     services = {
       gvfs.enable = true;
